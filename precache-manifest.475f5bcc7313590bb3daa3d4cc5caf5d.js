aee2e5faf3a17bdc186859fc55dc88a5994f9d87
ThomasLuu00 <Thomas.Luu00@gmail.com>
2020-01-30 19:18:10 -0500

    initial commit

---
+++ precache-manifest.475f5bcc7313590bb3daa3d4cc5caf5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'df2db02ff9021e42123823335700911e',
+    url: '/my-app/index.html'
+  },
+  {
+    revision: 'c11483fe9be858121607',
+    url: '/my-app/static/css/main.de254b7a.chunk.css'
+  },
+  {
+    revision: '9e44a3465cf43a28b4af',
+    url: '/my-app/static/js/2.4f2ff7da.chunk.js'
+  },
+  {
+    revision: '9652715b819d26dcc09aad3261a56a36',
+    url: '/my-app/static/js/2.4f2ff7da.chunk.js.LICENSE'
+  },
+  {
+    revision: 'c11483fe9be858121607',
+    url: '/my-app/static/js/main.1222b8e4.chunk.js'
+  },
+  {
+    revision: '76087bd5098e6c47cc02',
+    url: '/my-app/static/js/runtime-main.e129bf71.js'
+  },
+  {
+    revision: '2d01c3a847c78adbea63152e8e85a930',
+    url: '/my-app/static/media/simpleSnails.2d01c3a8.ttf'
+  }
+])
